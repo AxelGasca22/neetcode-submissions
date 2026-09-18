776d2581bb752523c5e78eeb0562f96860946b1c
Garcia Gasca Axel Gabriel <152086789+AxelGasca22@users.noreply.github.com>
2026-09-17 19:02:24 -0600

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minValue=prices[0];
+        int maxValue=0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minValue){
+                minValue = prices[i];
+            }
+            else{
+                if((prices[i] - minValue) > maxValue){
+                    maxValue = prices[i] - minValue;
+                }
+            }
+        }
+
+        return maxValue;
+
+        
+}
+
+}
